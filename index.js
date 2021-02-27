a07b73ab5db8c47577b7722529ac1f26edd72ed1
peter Szabo-toth <peterszabototh@gmail.com>
2021-02-27 19:42:34 +0100

    thatAll

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const app = express();
+// in the imports above
+const fs = require("fs");
+const videoName = "/video.mkv"
+
+let syncTime = {};
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/sync", function (req, res) {
+  //expect an ID a leader and a time
+  const sessionID = req.headers.sessionid;
+  const leader = req.headers.isleader;
+  const time = parseFloat(req.headers.time);
+  const isPaused = req.headers.ispaused;
+
+  let resPonseTime;
+
+  if (leader === "true") {
+    syncTime[sessionID] = {};
+    syncTime[sessionID]["CurrentTime"] = time;
+    syncTime[sessionID]["CurrentIsPaused"] = isPaused;
+    resPonseTime = syncTime[sessionID]["CurrentTime"];
+    responsePaused = syncTime[sessionID]["CurrentIsPaused"];
+  } else if (leader === "false") {
+    resPonseTime = syncTime[sessionID]["CurrentTime"];
+    responsePaused = syncTime[sessionID]["CurrentIsPaused"];
+  }
+ 
+
+  res.send(`${resPonseTime}|${responsePaused}`);
+});
+
+app.listen(8000, function () {
+  console.log("Listening on port 8000!");
+});
+
+app.get("/video", function (req, res) {
+  // Ensure there is a range given for the video
+  const range = req.headers.range;
+  if (!range) {
+    res.status(400).send("Requires Range header");
+  }
+
+  // get video stats (about 61MB)
+  const videoPath = __dirname + videoName;
+  const videoSize = fs.statSync(videoPath).size;
+
+  // Parse Range
+  // Example: "bytes=32324-"
+  const CHUNK_SIZE = 10 ** 6; // 1MB
+  const start = Number(range.replace(/\D/g, ""));
+  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+  // Create headers
+  const contentLength = end - start + 1;
+  const headers = {
+    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+    "Accept-Ranges": "bytes",
+    "Content-Length": contentLength,
+    "Content-Type": "video/mp4",
+  };
+
+  // HTTP Status 206 for Partial Content
+  res.writeHead(206, headers);
+
+  // create video read stream for this particular chunk
+  const videoStream = fs.createReadStream(videoPath, { start, end });
+
+  // Stream the video chunk to the client
+  videoStream.pipe(res);
+});
